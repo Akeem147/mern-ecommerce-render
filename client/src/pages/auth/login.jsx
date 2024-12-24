07ccd20ec102a88c760e092373b7628c8f01c755
Herbert Tiwatope <herberttiwatope@gmail.com>
2024-12-24 12:12:45 +0100

    Modified the toast notification

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -6,7 +6,6 @@ import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
 
-
 const initialState = {
   email: "",
   password: "",
@@ -14,44 +13,44 @@ const initialState = {
 function AuthLogin() {
   const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
-  
+
   function onSubmit(event) {
     event.preventDefault();
     dispatch(loginUser(formData)).then((data) => {
       if (data?.payload?.success) {
-        toast.success({ title: data?.payload?.message})
+        toast.success("Successful", { title: data?.payload?.message });
       } else {
-        toast.error({ title: data?.payload?.message})
+        toast.error("Unable to register", { title: data?.payload?.message });
       }
     });
   }
   return (
-     <>
+    <>
       <Toaster />
-    <div className="mx-auto w-full max-w-md space-y-6">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          Login to your account
-        </h1>
-        <p className="mt-2">
-          Dont have an account?
-          <Link
-            className="font-medium text-primary hover:underline ml-2"
-            to="/auth/register"
-          >
-            Register
-          </Link>
-        </p>
+      <div className="mx-auto w-full max-w-md space-y-6">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold tracking-tight text-foreground">
+            Login to your account
+          </h1>
+          <p className="mt-2">
+            Dont have an account?
+            <Link
+              className="font-medium text-primary hover:underline ml-2"
+              to="/auth/register"
+            >
+              Register
+            </Link>
+          </p>
+        </div>
+        <CommonForm
+          formControls={loginFormControls}
+          buttonText="Sign In"
+          formData={formData}
+          setFormData={setFormData}
+          onSubmit={onSubmit}
+        />
       </div>
-      <CommonForm
-        formControls={loginFormControls}
-        buttonText="Sign In"
-        formData={formData}
-        setFormData={setFormData}
-        onSubmit={onSubmit}
-      />
-    </div>
-     </>
+    </>
   );
 }
 
--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -16,48 +16,46 @@ function AuthRegister() {
   const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  
 
   function onSubmit(event) {
     event.preventDefault();
     dispatch(registerUser(formData)).then((data) => {
       if (data?.payload?.success) {
-        toast.success({ title: data?.payload?.message})
+        toast.success("Successful", { title: data?.payload?.message });
         navigate("/auth/login");
       } else {
-        
-        toast.error({ title: data?.payload?.message})
+        toast.error("Unable to register", { title: data?.payload?.message });
       }
     });
   }
 
   return (
-   <>
-    <Toaster />
-    <div className="mx-auto w-full max-w-md space-y-6">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          Create a new account
-        </h1>
-        <p className="mt-2">
-          Already have an account?
-          <Link
-            className="font-medium text-primary hover:underline ml-2"
-            to="/auth/login"
-          >
-            Login
-          </Link>
-        </p>
+    <>
+      <Toaster />
+      <div className="mx-auto w-full max-w-md space-y-6">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold tracking-tight text-foreground">
+            Create a new account
+          </h1>
+          <p className="mt-2">
+            Already have an account?
+            <Link
+              className="font-medium text-primary hover:underline ml-2"
+              to="/auth/login"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+        <CommonForm
+          formControls={registerFormControls}
+          buttonText="Sign Up"
+          formData={formData}
+          setFormData={setFormData}
+          onSubmit={onSubmit}
+        />
       </div>
-      <CommonForm
-        formControls={registerFormControls}
-        buttonText="Sign Up"
-        formData={formData}
-        setFormData={setFormData}
-        onSubmit={onSubmit}
-      />
-    </div>
-   </>
+    </>
   );
 }
 
